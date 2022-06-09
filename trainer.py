e7ea603866c22192399cf86f807b8839aecedde0
julschoen <31760205+julschoen@users.noreply.github.com>
2022-06-09 10:49:56 +0200

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -237,7 +237,7 @@ class Trainer(object):
         with autocast():
             z = torch.randn(real.shape[0], self.p.z_size, dtype=torch.float, device=self.device)
             fake = self.imG(z)
-            print(fake.shape, real.shape)
+
             disc_fake = self.imD(fake)
             disc_real = self.imD(real.unsqueeze(1))
         errD_real = (nn.ReLU()(1.0 - disc_real)).mean()
